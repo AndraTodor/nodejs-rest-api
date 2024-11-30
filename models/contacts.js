4172fa85a40f3de855aa123115f3cea51185dd08
AndraTodor <andra_t27@yahoo.com>
2024-11-30 19:10:15 +0200

    Rezolvarea temei hw03:REST API si conectarea MongoDb folosind mongoose

--- models/contacts.js
+++ models/contacts.js
@@ -1,47 +1,28 @@
-const fs = require("fs/promises");
-const path = require("path");
 const { v4: uuidv4 } = require("uuid");
-const contactsPath = path.join(__dirname, "contacts.json");
+const Contact = require("./contact");
 
 const listContacts = async () => {
-  const data = await fs.readFile(contactsPath);
-  return JSON.parse(data);
+  return await Contact.find();
 };
 
 const getContactById = async (contactId) => {
-  const contacts = await listContacts();
-  return contacts.find((contact) => contact.id === contactId);
+  return await Contact.findById(contactId);
 };
 
 const removeContact = async (contactId) => {
-  const contacts = await listContacts();
-  const updatedContacts = contacts.filter(
-    (contact) => contact.id !== contactId
-  );
-  if (contacts.length === updatedContacts.length) {
-    return null;
-  }
-  await fs.writeFile(contactsPath, JSON.stringify(updatedContacts));
-  return { message: "Contact deleted" };
+  return await Contact.findByIdAndRemove(contactId);
 };
 
 const addContact = async (body) => {
-  const contacts = await listContacts();
-  const newContact = { id: uuidv4(), ...body };
-  contacts.push(newContact);
-  await fs.writeFile(contactsPath, JSON.stringify(contacts));
-  return newContact;
+  return await Contact.create(body);
 };
 
 const updateContact = async (contactId, body) => {
-  const contacts = await listContacts();
-  const index = contacts.findIndex((contact) => contact.id === contactId);
-  if (index === -1) {
-    return null;
-  }
-  contacts[index] = { ...contacts[index], ...body };
-  await fs.writeFile(contactsPath, JSON.stringify(contacts));
-  return contacts[index];
+  return await Contact.findByIdAndUpdate(contactId, body, { new: true });
+};
+
+const updateStatusContact = async (contactId, body) => {
+  return await Contact.findByIdAndUpdate(contactId, body, { new: true });
 };
 
 module.exports = {
@@ -50,4 +31,5 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
+  updateStatusContact,
 };
