8db85494692394877c971c241f3d18d8041b67e0
AndraTodor <andra_t27@yahoo.com>
2025-01-14 15:26:52 +0200

    Add changes for hw05-avatars plus Jest test

--- app.js
+++ app.js
@@ -2,20 +2,20 @@ const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
 const mongoose = require("mongoose");
-require("dotenv").config(); //variabile de mediu
+require("dotenv").config();
 
 const authRouter = require("./routes/api/authRoutes");
 const contactsRouter = require("./routes/api/contactRoutes");
+const userRouter = require("./routes/api/userRoutes");
 
 const app = express();
+
+app.use(express.static("public"));
 
 const { DB_HOST } = process.env;
 
 mongoose
-  .connect(DB_HOST, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(DB_HOST)
   .then(() => console.log("Database connection successful"))
   .catch((error) => {
     console.error(error.message);
@@ -30,6 +30,7 @@ app.use(express.json());
 
 app.use("/api/contacts", contactsRouter);
 app.use("/api/auth", authRouter);
+app.use("/api/users", userRouter);
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
